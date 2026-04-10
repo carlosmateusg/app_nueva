db1ead03448f9124ce4025bbed4dd8ca125b8664
carlosmateusg <ingcarlos.mateus@gmail.com>
2026-04-09 21:30:14 -0500

    feat: error

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main(){
 }
 
 class login extends StatelessWidget{
-  login({super.key});
+  const login({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -13,14 +13,20 @@ class login extends StatelessWidget{
     return MaterialApp(
       debugShowCheckedModeBanner: false, //Esto es para quitar el banner de debug.
       home: Scaffold(   
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 46, 205, 128),
+          foregroundColor: Colors.yellowAccent,
+          shadowColor: Colors.tealAccent,
+          elevation: 10,
+          centerTitle: true, //Esto es para centrar el título en la barra de navegación.
+          title: Text("Nuevo Depórte"),
+        ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center, //Esto es para centrar el contenido en la pantalla.
-          children:[
-            Text("Hola Mundo",  
-            style: TextStyle(fontSize: 24), //Style nos ayua a modificar el texto. 
-          ),
-          Text("User",
+        children:[
+          Image.network(height: 50, "https://static.vecteezy.com/system/resources/thumbnails/012/996/773/small/sport-ball-football-free-png.png"),
+          Text("Hola Mundo!",
           style: TextStyle(fontSize: 18), 
           ),
           Row(
@@ -33,13 +39,16 @@ class login extends StatelessWidget{
               child: Text("Click me too!")),
             ],
           )
-         ],
+        ],
         ),
         )
+      floatingActionButton: FloatingActionButton( //Esto es para crear un botón flotante, el onPressed es para definir la acción que se ejecutará cuando se presione el botón, en este caso no hace nada.
+        onPressed: ()=>{
+          print("Clicke me bro!"),
+        },
+        child: Icon(Icons.plus_one), //Esto es para agregar un ícono al botón flotante.
     )
+      )
     );
   }
-
-
-
 }
