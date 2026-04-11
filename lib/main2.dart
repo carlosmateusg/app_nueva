a1e0da054f47b048e295ecb0b8d077290e073127
carlosmateusg <ingcarlos.mateus@gmail.com>
2026-04-10 20:16:11 -0500

    feat widget cambia

---
+++ lib/main2.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main (){
+  runApp(app);
+}
+
+class app extends statetlessWidget{ //widget no cambia 
+
+}
+
+class loginScreen extends statefulwidget{ //Acá si cambia el widget, por ejemplo, si queremos cambiar el texto de un botón al hacer clic en él, necesitamos usar un statefulwidget.
+
+}
