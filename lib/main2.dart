91095769aef99dfb39eab65cb761c004faa3e7c1
carlosmateusg <ingcarlos.mateus@gmail.com>
2026-04-10 20:45:21 -0500

    feat: button

--- lib/main2.dart
+++ lib/main2.dart
@@ -1,13 +1,64 @@
 import 'package:flutter/material.dart';
 
 void main (){
-  runApp(app);
+  runApp(App());
 }
 
-class app extends statetlessWidget{ //widget no cambia 
+class App extends StatelessWidget{//para una pantalla se hace cada class 
+  const App({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Bienvenido"),
+          centerTitle: true,
+          foregroundColor: Colors.white,
+          backgroundColor: Colors.orange,
+        ),
+        body: Text("Pantalla 1"),
+      ),
+    );
+  }
 }
 
-class loginScreen extends statefulwidget{ //Acá si cambia el widget, por ejemplo, si queremos cambiar el texto de un botón al hacer clic en él, necesitamos usar un statefulwidget.
+class LoginScreen extends StatefulWidget{ //Acá si cambia el widget, por ejemplo, si queremos cambiar el texto de un botón al hacer clic en él, necesitamos usar un statefulwidget.
+  const LoginScreen({super.key});
 
+  @override 
+  State<LoginScreen> createState() => _LoginScreenState(); //wiget si cambia 
+
+  }
+
+  class _LoginScreenState extends State<LoginScreen>{
+    @override 
+    Widget build(BuildContext context) {
+      
+      return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Login2"),
+            centerTitle: true,
+            backgroundColor: Colors.lightBlue,
+            foregroundColor: Colors.white,
+          ),
+          body: Column(
+            children:[
+              ElevatedButton(
+                onPressed: onPressed,
+              child: child
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.lightBlue,
+                foregroundColor: Colors.red,
+              )
+              )
+            ]
+          )
+        ),
+      );
+    }
+    
 }
+
+
